266af2ae1794cfd9de161ed0f70870083e1d941a
Chimtamreddy <kr7348202@gmail.com>
2024-03-23 11:53:46 +0530

    terraform

---
+++ 03-vars/sample.tf
@@ -0,0 +1,88 @@
+# Plain Variable
+variable "fruit_name" {
+  default = "apple"
+}
+
+output "fruit_name" {
+  value = var.fruit_name
+}
+
+# List Variable
+variable "fruits" {
+  default = [
+    "mango",
+    "banana"
+  ]
+}
+
+output "fruits" {
+  value = var.fruits
+}
+
+# Map Variable
+variable "fruit_stock" {
+  default = {
+    kiwi = 100
+    orange = 200
+  }
+}
+
+output "fruit_stock" {
+  value = var.fruit_stock
+}
+
+# Map Variable, Map of Maps
+variable "fruit_stock_with_price" {
+  default = {
+    kiwi = {
+      stock = 10
+      price = 20
+    }
+
+    orange = {
+      stock = 100
+      price = 10
+    }
+
+  }
+}
+
+
+## Access a list variable, List index start from zero
+
+output "fruit_first" {
+  value = var.fruits[0]
+}
+
+output "fruit_second" {
+  value = var.fruits[1]
+}
+
+# Access a Map Variable
+output "fruit_stock_orange" {
+  value = var.fruit_stock["orange"]
+}
+
+output "fruit_stock_with_price_of_kiwi" {
+  value = var.fruit_stock["kiwi"].stock
+}
+
+# Variable Data Types
+variable "fruit_details" {
+  default = {
+    apple = {
+      stock = 100 # number
+      type = "washington" # string
+      for_sale = true #boolean
+    }
+  }
+}
+
+# Variable in a combination of any other string then it needs to be with in ${}
+output "fruit_name_1" {
+  value = "Fruit Name = ${var.fruit_name}"
+}
+
+output "fruit_details" {
+  value = "Apple Stock = ${var.fruit_details[apple].stock}, Apple Type = ${var.fruit_details[apple].type}, Apple Sale Status = ${var.fruit_details[apple].for_sale}"
+}
---
+++ roboshop-v1/roboshop.tf
@@ -0,0 +1,98 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-061167548882a91e3"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z05847051QFB84Q2A5JP0"
+  name    = "frontend-dev.kr7348202.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
+  }
+}
