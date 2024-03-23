c54ba592999708eb5e2390feef57ec550e84eb99
Chimtamreddy <kr7348202@gmail.com>
2024-03-23 12:08:36 +0530

    terraform

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -77,5 +77,5 @@ output "fruit_name_1" {
 }
 
 output "fruit_details_apple" {
-  value = "Apple Stock = ${var.fruit_details[apple].stock}, Apple Type = ${var.fruit_details[apple].type}, Apple Sale Status = ${var.fruit_details[apple].for_sale}"
+  value = "Apple Stock = ${var.fruit_details["apple"].stock}, Apple Type = ${var.fruit_details["apple"].type}, Apple Sale Status = ${var.fruit_details["apple"].for_sale}"
 }
