4626777e5d67ea61c67c80eb9f22a80162db6634
Chimtamreddy <kr7348202@gmail.com>
2024-03-23 11:59:59 +0530

    terraform

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -16,7 +16,7 @@ variable "fruits" {
 }
 
 output "fruits" {
-  value = var.fruits[0]
+  value = var.fruits
 }
 
 # Map Variable
@@ -31,4 +31,38 @@ output "fruit_stock" {
   value = var.fruit_stock
 }
 
+# Map Variable, Map of Maps
+variable "fruit_stock_with_price" {
+  default = {
+    kiwi = {
+      stock = 10
+      price = 20
+    }
 
+    orange = {
+      stock = 100
+      price = 10
+    }
+
+  }
+}
+
+
+## Access a list variable, List index start from zero
+
+output "fruit_first" {
+  value = var.fruits[0]
+}
+
+output "fruit_second" {
+  value = var.fruits[1]
+}
+
+# Access a Map Variable
+output "fruit_stock_orange" {
+  value = var.fruit_stock["orange"]
+}
+
+output "fruit_stock_with_price_of_kiwi" {
+  value = var.fruit_stock["kiwi"].stock
+}
