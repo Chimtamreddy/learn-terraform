d3c187c74d860f90ead33a6c8d6464ca0caff5f5
Chimtamreddy <kr7348202@gmail.com>
2024-03-23 12:29:39 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -3,25 +3,24 @@ variable "fruits" {
 }
 
 output "fruits" {
-  value = var.fruits[0]
-
+#  value = var.fruits[0]
+  value = element(var.fruits, 2)
 }
 
 variable "fruit_with_stock" {
   default = {
     apple = {
       stock = 10
-      price = 30
     }
   }
 }
 
 output "fruit_stock" {
-  value = var.fruit_with_stock
-
+  value = var.fruit_with_stock[apple]
+#  value = try(var.fruit_with_stock[banana], 0)
 }
 
 output "fruit_stock_price" {
   value = lookup(var.fruit_with_stock, "apple", 30)
-
+#  value = try(var.fruit_with_stock[apple].price, 25)
 }
