6bad5d38ef0be0419c4eea1925b5cf2e0e8c6294
Chimtamreddy <kr7348202@gmail.com>
2024-03-23 12:24:22 +0530

    terraform

---
+++ 04-functions/main.tf
@@ -0,0 +1,26 @@
+variable "fruits" {
+  default = ["apple", "banana"]
+}
+
+output "fruits" {
+  value = var.fruits[0]
+
+}
+
+variable "fruit_with_stock" {
+  default = {
+    apple = {
+      stock = 10
+    }
+  }
+}
+
+output "fruit_stock" {
+  value = var.fruit_with_stock[apple]
+
+}
+
+output "fruit_stock_price" {
+  value = lookup(var.fruit_with_stock, "apple", 30)
+
+}
