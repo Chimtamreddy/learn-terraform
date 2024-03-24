19b025f6a81f54b3a299f7f4db14ae9403e4033e
Chimtamreddy <kr7348202@gmail.com>
2024-03-24 11:27:22 +0530

    terraform

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,14 +1,14 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
   name_regex    = "Centos-*"
   owners = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
 
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami_ids.ami
+  ami           = data.aws_ami.ami.id
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-061167548882a91e3"]
 
