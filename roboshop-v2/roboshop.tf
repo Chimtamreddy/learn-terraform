1132bce87d73279cd43e589cfea76b3ff1e72de3
Chimtamreddy <kr7348202@gmail.com>
2024-03-24 11:02:07 +0530

    terraform

--- roboshop-v2/roboshop.tf
+++ roboshop-v2/roboshop.tf
@@ -14,20 +14,36 @@ variable "zone_id" {
   default = "Z05847051QFB84Q2A5JP0"
 }
 
-resource "aws_instance" "frontend" {
+variable "components" {
+  default = {
+    frontend = { name = "frontend-dev" }
+    mongodb = { name = "mongodb-dev" }
+    catalogue = { name = "catalogue-dev" }
+    redis = { name = "redis-dev" }
+    user = { name = "user-dev" }
+    cart = { name = "cart-dev" }
+    mysql = { name = "mysql-dev" }
+    shipping = { name = "shipping-dev" }
+    rabbitmq = { name = "rabbitmq-dev" }
+    payment = { name = "payment-dev" }
+  }
+}
+
+resource "aws_instance" "instance" {
+  for_each = var.components
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.security_groups
 
   tags = {
-    Name = "frontend"
+    Name = lookup(var.components, each.key, null)
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = var.zone_id
-  name    = "frontend-dev.kr7348202.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
+#resource "aws_route53_record" "frontend" {
+#  zone_id = var.zone_id
+#  name    = "frontend-dev.kr7348202.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.frontend.private_ip]
+#}
