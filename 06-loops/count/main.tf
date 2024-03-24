e042d32a4639d18570e60cb68b1a724408cf24f4
Chimtamreddy <kr7348202@gmail.com>
2024-03-24 10:42:06 +0530

    terraform

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,15 @@
+variable "components" {
+  default = [ "frontend", "mongodb" ]
+}
+
+resource "aws_instance" "instance" {
+  count = length(var.components)
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-061167548882a91e3"]
+
+  tags = {
+    Name = ""
+
+  }
+}
