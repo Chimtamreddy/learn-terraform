48969f33b5770115932708b9c917ef54057608b7
Chimtamreddy <kr7348202@gmail.com>
2024-03-25 13:41:13 +0530

    terraform

--- sg.tf
+++ sg.tf
@@ -20,3 +20,31 @@
 #    ipv6_cidr_blocks = ["::/0"]
 #  }
 #}
+
+# Another Security Group
+
+#resource "aws_security_group" "main" {
+#  name        = "${local.name_prefix}-sg"
+#  description = "${local.name_prefix}-sg"
+#  vpc_id      = var.vpc_id
+#
+#  tags        = merge(local.tags, { Name = "${local.name_prefix}-sg" })
+#}
+#
+#resource "aws_vpc_security_group_ingress_rule" "main" {
+#  security_group_id = aws_security_group.main.id
+#  description       = "SSH"
+#  cidr_ipv4         = var.ssh_ingress_cidr
+#  from_port         = 22
+#  ip_protocol       = "tcp"
+#  to_port           = 22
+#}
+#
+#
+#
+#resource "aws_vpc_security_group_egress_rule" "main" {
+#  security_group_id = aws_security_group.main.id
+#  cidr_blocks      = ["0.0.0.0/0"]
+#  cidr_ipv6         = "::/0"
+#  ip_protocol       = "-1" # semantically equivalent to all ports
+#}
