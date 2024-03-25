7c52c3a662ac1e6a24e000eb8c9a9a9259ff79b3
Chimtamreddy <kr7348202@gmail.com>
2024-03-25 16:42:30 +0530

    terraform

--- sg.tf
+++ sg.tf
@@ -48,3 +48,37 @@
 #  cidr_ipv6         = "::/0"
 #  ip_protocol       = "-1" # semantically equivalent to all ports
 #}
+
+#resource "aws_instance" "main" {
+#  ami           = "ami-0f3c7d07486cad139"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#  subnet_id = local.app_subnet_ids[0]
+#}
+#
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic and all outbound traffic"
+#  vpc_id      = aws_vpc.main.id
+#
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#
+#
+#  ingress {
+#    description = "SSH"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#}
