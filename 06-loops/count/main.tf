a18757ce7d4428d249a7155539a68f08f1f38e39
Chimtamreddy <kr7348202@gmail.com>
2024-04-05 19:45:22 +0530

    learn terraform

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,17 +1,21 @@
 variable "components" {
-  default = [ "catalogue", "frontend", "mongodb" ]
+  default = [ "frontend", "mongodb" ]
 }
 
-resource "aws_instance" "instance" {
+#resource "aws_instance" "instance" {
+#  count = length(var.components)
+#  ami           = "ami-0f3c7d07486cad139"
+#  instance_type = "t3.small"
+#  vpc_security_group_ids = ["sg-061167548882a91e3"]
+#
+#  tags = {
+##    Name = var.components[count.index]
+#    Name = element(var.components, count.index)
+#
+#  }
+#}
+
+resource "aws_security_group" "allow_tls" {
   count = length(var.components)
-  ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0cd45286220a14e5a"]
-
-  tags = {
-    Name = var.components[count.index]
-
-
-  }
+  name = var.components[count.index]
 }
-
