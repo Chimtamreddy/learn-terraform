765f008fdf97650eeef9806e36361bba7b7b0fb4
Chimtamreddy <kr7348202@gmail.com>
2024-04-08 09:30:46 +0530

    roboshop terraform

--- 10-map-test/vars.tf
+++ 10-map-test/vars.tf
@@ -5,7 +5,7 @@ variable vpc {
       subnets = {
         public = {
           public1 = { cidr = "10.0.0.0/16", az = "us-east-1a" }
-          public12 = { cidr = "10.0.1.0/16", az = "us-east-1b" }
+          public2 = { cidr = "10.0.1.0/16", az = "us-east-1b" }
         }
         app = {
           app1 = { cidr = "10.0.2.0/16", az = "us-east-1a" }
