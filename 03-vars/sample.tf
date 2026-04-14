383a7b02d709b7d7716daa5dc0b8bc89deb603bb
chimtamreddy <7348202@gmail.com>
2026-04-14 21:19:35 +0530

    variables in terraform

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -14,3 +14,15 @@ variable "fruit_stock" {
 output "fruit" {
   value = var.fruit_stock
 }
+
+# Print List Variables
+variable "fruit_with_stock" {
+  default = ["apple", "banana" ]
+}
+output "fruit_with_stock" {
+  value = var.fruit_with_stock[0]
+}
+
+output "fruit_with_stock_1" {
+  value = var.fruit_with_stock[1]
+}
