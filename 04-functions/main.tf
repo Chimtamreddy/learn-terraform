78a98dc1094c15d67b93620451ba6e76feeaac7f
chimtamreddy <7348202@gmail.com>
2026-04-14 21:51:21 +0530

    Functions in terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -4,5 +4,19 @@ variable "components" {
 
 output "components" {
 #  value = var.components[0]
-  value = element(var.components, 3)
+  value = elements(var.components, 3)
+}
+
+variable "fruits_with_stock" {
+  default = {
+    apple = {
+      stock = 100
+      price = 20
+    }
+  }
+}
+
+output "fruits_with_stock" {
+#  value = var.fruits_with_stock["apple"].stock
+  value = try(var.fruits_with_stock["apple"], price)
 }
