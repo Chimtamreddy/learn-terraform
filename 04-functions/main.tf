8f5276464a43491b83af4229358714b6dd6e4a55
chimtamreddy <7348202@gmail.com>
2026-04-14 21:56:37 +0530

    Functions in terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -23,5 +23,5 @@ output "fruits_with_stock" {
 
 output "fruits_with_stock_1" {
   #  value = var.fruits_with_stock["apple"].stock
-  value = nslookup(var.fruits_with_stock["apple"], "banana" 2)
+  value = nslookup(var.fruits_with_stock["apple"], "banana", 2)
 }
