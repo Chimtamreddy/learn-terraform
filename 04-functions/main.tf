71a7004d0fe0c4726473c93929ac9272bc354101
chimtamreddy <7348202@gmail.com>
2026-04-14 21:54:37 +0530

    Functions in terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -20,3 +20,8 @@ output "fruits_with_stock" {
 #  value = var.fruits_with_stock["apple"].stock
   value = try(var.fruits_with_stock["banana"], 0)
 }
+
+output "fruits_with_stock_1" {
+  #  value = var.fruits_with_stock["apple"].stock
+  value = nslookup(var.fruits_with_stock, "banana" 2)
+}
