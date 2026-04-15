f76f8030f8a446907bee69d04da82aa54a85b4e7
chimtamreddy <7348202@gmail.com>
2026-04-15 20:33:27 +0530

    git

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,16 +1,28 @@
-resource "aws_instance" "frontend" {
-  ami           = "ami-0b4f379183e5706b9"
+variable "ami" {
+  ami = "ami-0b4f379183e5706b9"
+}
+variable "instance_type" {
   instance_type = "t3.small"
+}
+variable "security_groups" {
   vpc_security_group_ids = [
     "sg-0e3a1d089c9059cfc"
   ]
+}
+variable "zone_id" {
+  zone_id = "Z08590761XW1O9G92270H"
+}
+resource "aws_instance" "frontend" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
   tags = {
     Name = "frontend"
   }
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z08590761XW1O9G92270H"
+  zone_id = var.zone_id
   name    = "frontend-dev.anysite.info"
   type    = "A"
   ttl     = 30
