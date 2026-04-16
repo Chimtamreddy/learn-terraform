659663b8828e20d96c85c861d15849d61ba77a6a
chimtamreddy <7348202@gmail.com>
2026-04-16 19:32:57 +0530

    Modules

--- 07-module/local-module/main.tf
+++ 07-module/local-module/main.tf
@@ -1,9 +1,19 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
+  most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
   owners     = ["973714476881"]
 
 }
 
 output "aws_ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami.id
 }
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ "sg-0e3a1d089c9059cfc" ]
+
+}
+
+variable "instance_type" {}
