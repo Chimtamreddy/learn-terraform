1d11f5dcdea89e902cdc2722a9849414e851efd5
chimtamreddy <7348202@gmail.com>
2026-04-16 17:48:33 +0530

    Count Loop in teraform

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,16 +1,23 @@
 variable "components" {
-  default = ["redis", "frontend", "mongodb", "catalogue"]
+  default = ["frontend", "mongodb", "catalogue"]
 }
 
-resource "aws_instance" "instance" {
+# resource "aws_instance" "instance" {
+#   count = length(var.components)
+#   ami           = "ami-0b4f379183e5706b9"
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = [
+#     "sg-0e3a1d089c9059cfc"
+#   ]
+#   tags = {
+#     Name = element(var.components, count.index)
+#   }
+# }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
   count = length(var.components)
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [
-    "sg-0e3a1d089c9059cfc"
-  ]
   tags = {
     Name = element(var.components, count.index)
   }
 }
-
