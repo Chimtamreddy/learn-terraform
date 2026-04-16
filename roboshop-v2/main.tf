6d441f2414cfb4800ec8df126074ceebaf7b7782
chimtamreddy <7348202@gmail.com>
2026-04-16 18:43:17 +0530

    Count Loop in teraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -33,14 +33,14 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "aws_route53_record" "record" {
-  for_each = var.components
-  zone_id = var.zone_id
-  name    = "${lookup(each.value, "name", null)}.anysite.info"
-  type    = "A"
-  ttl     = 30
-  # records = lookup(aws_instance.instance, )
-}
+# resource "aws_route53_record" "record" {
+#   for_each = var.components
+#   zone_id = var.zone_id
+#   name    = "${lookup(each.value, "name", null)}.anysite.info"
+#   type    = "A"
+#   ttl     = 30
+#   # records = lookup(aws_instance.instance, )
+# }
 
 output "aws_instance" {
   value = aws_instance.instance
