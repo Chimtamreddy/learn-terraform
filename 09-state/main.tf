fc61a54faff3a01bdc425e1df475bca94ef96266
chimtamreddy <7348202@gmail.com>
2026-04-18 15:12:56 +0530

    Terraform tfvars

--- 09-state/main.tf
+++ 09-state/main.tf
@@ -1,13 +1,15 @@
-terraform {
-  backend "s3" {
-    bucket = "tf-state-db01"
-    key    = "09-state/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-variable "test" {}
-
-output "test" {
-  value = var.test
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "tf-state-db01"
+#     key    = "09-state/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+#
+# variable "test" {
+#   default = "hello"
+# }
+#
+# output "test" {
+#   value = var.test
+# }
