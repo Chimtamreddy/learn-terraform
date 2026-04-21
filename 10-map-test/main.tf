25c4e89b1b94e38699cce711c724614f167ed79d
chimtamreddy <7348202@gmail.com>
2026-04-21 22:00:20 +0530

    Creating VPC

---
+++ 10-map-test/main.tf
@@ -0,0 +1,25 @@
+vpc = {
+  default = {
+    main = {
+      cidr = "10.0.0.0/16"
+      subnets = {
+        public = {
+          public1 = { cidr = "10.0.0.0/24", az = "us-east-1a" }
+          public2 = { cidr = "10.0.1.0/24", az = "us-east-1b" }
+        }
+        public = {
+          public1 = { cidr = "10.0.2.0/24", az = "us-east-1a" }
+          public1 = { cidr = "10.0.3.0/24", az = "us-east-1b" }
+        }
+        public = {
+          public1 = { cidr = "10.0.4.0/24", az = "us-east-1a" }
+          public1 = { cidr = "10.0.5.0/24", az = "us-east-1b" }
+        }
+      }
+    }
+  }
+}
+
+output "all_subnets" {
+  value = { for k,v in vpc["main"]["subnets"]: k => v}
+}
